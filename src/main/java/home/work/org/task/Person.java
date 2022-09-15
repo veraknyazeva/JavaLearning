0ffd05886e7efdd31e408be4b8b981069819c90d
”Vera <verashibanova@yandex.ru>
2022-09-15 20:38:08 +0500

    Добавила git.ignore.file

--- src/main/java/home/work/org/task/Person.java
+++ src/main/java/home/work/org/task/Person.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class Person {
     private String name;
     private Sex sex;
+
     private Date birthDate;
 
     private Person(String name, Sex sex, Date birthDate) {
